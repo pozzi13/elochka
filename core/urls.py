2627a559bff3567c43d7b1e60bf96243dfcd7f23
federation22 <104779542+pozzi13@users.noreply.github.com>
2026-03-13 13:07:40 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -5,17 +5,15 @@ app_name = "core"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("uslugi-i-ceny/", views.services_prices, name="services_prices"),
+    path("kontakty-i-otzyvy/", views.contacts_reviews, name="contacts_reviews"),
 
-    # 3 пользовательские страницы:
     path("reviews/", views.reviews_page, name="reviews"),
-    path("review/", views.review_create, name="review_create"),
     path("contacts/", views.contacts_page, name="contacts"),
 
-    # формы
     path("booking/", views.booking_create, name="booking_create"),
     path("review/", views.review_create, name="review_create"),
 
-    # техничка
     path("sitemap.xml", views.sitemap_xml, name="sitemap_xml"),
     path("sitemap.html", views.sitemap_html, name="sitemap_html"),
     path("robots.txt", views.robots_txt, name="robots_txt"),
